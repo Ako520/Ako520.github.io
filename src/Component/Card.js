c3c55b10eef186ff3f4df86dde4ed7339f03c79a
DarkFlameMastaa <944644732@qq.com>
2016-10-21 10:53:29 +0800

    增加了文章发布时间等功能

---
+++ src/Component/Card.js
@@ -0,0 +1,91 @@
+import React, { PropTypes } from 'react'
+import {hashHistory} from 'react-router'
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+
+class OneCard extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      howlong:""
+    }
+  }
+
+  jump(x){
+    hashHistory.push(`/view/${x}`)
+  }
+  componentDidMount() {
+    // getCardData().then( (res) =>{
+    //   this.setState({
+    //     data:res,
+    //     wait:false
+    //   });
+    // } )
+    let createTime = this.props.createTime;
+    let par = /(\d+)-(\d+)-(\d+)T(\d+)/;
+    let arr = createTime.match(par);
+    let creatYear = arr[1];
+    let creatMonth = arr[2];
+    let creatDay = arr[3];
+    let creatHour = arr[4];
+
+    let curTime = new Date();
+    let curYear = curTime.getFullYear();
+    let curMonth = curTime.getMonth();
+    let curDay = curTime.getDate();
+    let curHour = curTime.getHours();
+    console.log(curTime,curYear,curMonth,curDay);
+    if (curYear > creatYear) {
+      this.setState({
+        howlong:`${curYear - creatYear}年前`
+      });
+    }
+    else if (curMonth > creatMonth) {
+      this.setState({
+        howlong:`${curMonth - creatMonth}月前`
+      });
+    }
+    else if (curDay > creatDay) {
+      this.setState({
+        howlong:`${curDay - creatDay}天前`
+      });
+    }
+    else if (curHour > creatHour) {
+      this.setState({
+        howlong:`${curHour - creatHour}小时前`
+      });
+    }
+    else if (curHour = creatHour) {
+      this.setState({
+        howlong: "刚刚"
+      });
+    }
+
+
+  }
+  render () {
+    let styles={
+      all:{
+        width:"80%",
+        maxWidth:"1200px",
+        boxShadow:"0 1px 6px rgba(0, 0, 0, 0.12), 0 1px 4px rgba(0, 0, 0, 0.24)",
+        bordrRadius:"20px",
+        margin:"0 auto 30px",
+        paddingBottom:"40PX"
+      }
+    }
+    let x=this.props.subtitle;let y="a<br/>b"
+    return(
+      <Card style={styles.all}>
+        <CardTitle title={this.props.title}  subtitle={this.state.howlong}/>
+        <CardText>
+          <div dangerouslySetInnerHTML={{__html:this.props.subtitle}}/>
+        </CardText>
+        <CardActions>
+          <FlatButton label="阅读全文" backgroundColor="#a4c639" hoverColor="#8AA62F" />
+        </CardActions>
+      </Card>
+    )
+  }
+}
+export default OneCard;
